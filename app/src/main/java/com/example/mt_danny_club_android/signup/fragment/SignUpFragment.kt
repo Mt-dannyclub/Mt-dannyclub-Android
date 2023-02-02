ab3a95a99c6055dabe9559fd8604ed315dd78026
stev3j <mygajeong@gmail.com>
2023-02-02 16:30:59 +0900

    add: Login&SignUp Screen

--- app/src/main/java/com/example/mt_danny_club_android/signup/fragment/SignUpFragment.kt
+++ app/src/main/java/com/example/mt_danny_club_android/signup/fragment/SignUpFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mt_danny_club_android.signup.fragment
 
+import android.graphics.Paint
 import com.example.mt_danny_club_android.R
 import com.example.mt_danny_club_android.base.BaseFragment
 import com.example.mt_danny_club_android.databinding.FragmentSignUpBinding
@@ -7,6 +8,9 @@ import com.example.mt_danny_club_android.databinding.FragmentSignUpBinding
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up)  {
 
     override fun start() {
-        TODO("Not yet implemented")
+        binding.tvLogin.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+        binding.tvLogin.setOnClickListener{
+            // login_fragment 로 이동
+        }
     }
 }
